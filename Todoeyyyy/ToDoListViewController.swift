76f3cd47b9dee79c49a0d7b3c1dfb40a2e8d030f
Mayank Gautam <Mayank Gautam>
2019-02-17 22:18:14 +0530

    ToDo List added to storage

--- Todoeyyyy/ToDoListViewController.swift
+++ Todoeyyyy/ToDoListViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
+    let ID_TO_DO_ITEM_CELL = "toDoItemCell"
+    let KEY_TO_DO_LIST = "ToDOList"
+    
+    var userDefaults = UserDefaults.standard
     var itemArray = ["Save Kenny", "Find Satan", "Buy XBox 360", "Fight Barbera Striesand"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let items = userDefaults.array(forKey: KEY_TO_DO_LIST) as? [String] {
+            itemArray = items
+        }
         // Do any additional setup after loading the view, typically from a nib.
 //        tableView.register(UINib(nibName: "ToDoItemCell", bundle: nil), forCellReuseIdentifier: "toDoItemCell")
     }
@@ -23,14 +32,13 @@ class ToDoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: ID_TO_DO_ITEM_CELL, for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
     
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
         let cell = tableView.cellForRow(at: indexPath)
         if cell?.accessoryType == .checkmark {
             cell?.accessoryType = .none
@@ -51,6 +59,7 @@ class ToDoListViewController: UITableViewController {
             // Mark what will happen
             self.itemArray.append(textField.text!)
             self.tableView.reloadData()
+            self.userDefaults.set(self.itemArray, forKey: self.KEY_TO_DO_LIST)
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
